9a3d5d42543768cfae7de86c5d1ab99832418f67
klinsc <klinscea@gmail.com>
2023-07-20 20:14:47 +0700

    Update index.tsx

--- src/components/UploadPDF/index.tsx
+++ src/components/UploadPDF/index.tsx
@@ -7,45 +7,40 @@ const UploadPDF = ({ setImageFile }: { setImageFile: (imageFile: File | null) =>
   // hooks
   const [fileList, setFileList] = useState<UploadFile[]>([])
   const [uploading, setUploading] = useState(false)
-  const [imageSrc, setImageSrc] = useState('')
-
-  // handlers
-  const handleUpload = () => {
-    const formData = new FormData()
-    fileList.forEach((file) => {
-      formData.append('files[]', file as RcFile)
-    })
-    setUploading(true)
-    // You can use any AJAX library you like
-    fetch('http://localhost:5000/upload', {
-      method: 'POST',
-      headers: {
-        enctype: 'multipart/form-data',
-        responseType: 'arraybuffer',
-      },
-      body: formData,
-    })
-      .then(async (res) => {
-        const blob = await res.blob()
-        const imageUrl = URL.createObjectURL(blob)
-        setImageSrc(imageUrl)
-
-        setImageFile(blob as File)
-      })
-      .then(() => {
-        setFileList([])
-        void message.success('upload successfully.')
-      })
-      .catch(() => {
-        void message.error('upload failed.')
-      })
-      .finally(() => {
-        setUploading(false)
-      })
-  }
 
   // props
   const props: UploadProps = {
+    customRequest: () => {
+      const formData = new FormData()
+      fileList.forEach((file) => {
+        formData.append('files[]', file as RcFile)
+      })
+      setUploading(true)
+      // You can use any AJAX library you like
+      fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        headers: {
+          enctype: 'multipart/form-data',
+          responseType: 'arraybuffer',
+        },
+        body: formData,
+      })
+        .then(async (res) => {
+          const blob = await res.blob()
+
+          setImageFile(blob as File)
+        })
+        .then(() => {
+          setFileList([])
+          void message.success('upload successfully.')
+        })
+        .catch(() => {
+          void message.error('upload failed.')
+        })
+        .finally(() => {
+          setUploading(false)
+        })
+    },
     onRemove: (file) => {
       const index = fileList.indexOf(file)
       const newFileList = fileList.slice()
@@ -68,27 +63,18 @@ const UploadPDF = ({ setImageFile }: { setImageFile: (imageFile: File | null) =>
 
       setFileList([...fileList, file])
 
-      return false
+      return true
     },
-    // onChange: (info) => {
-    //   console.log(info.fileList);
-    // },
     fileList,
   }
 
   return (
     <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
       <Upload {...props}>
-        <Button icon={<UploadOutlined />}>Select File</Button>
+        <Button icon={<UploadOutlined />} loading={uploading}>
+          Select File
+        </Button>
       </Upload>
-      <Button
-        type="primary"
-        onClick={handleUpload}
-        disabled={fileList.length === 0}
-        loading={uploading}
-        style={{ marginTop: 16 }}>
-        {uploading ? 'Uploading' : 'Start Upload'}
-      </Button>
     </Space>
   )
 }
