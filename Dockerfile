5c5ad904d9dcf5bb77b9eaeeb313061cb6c3953a
klinsc <klinscea@gmail.com>
2023-07-21 13:30:11 +0700

    added: ml.tsx

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# create a docker image for the application of t3 nextjs stack
+
+FROM node:lts-alpine3.14
+
+RUN apk update && \
+    apk add --no-cache libc6-compat autoconf automake libtool make tiff jpeg zlib zlib-dev pkgconf nasm file gcc musl-dev
+
+# Create app directory
+WORKDIR /usr/src/asdr-t3
+
+# Install app dependencies
+COPY package*.json ./
+
+RUN npm install -g npm
+
+ENV NEXT_TELEMETRY_DISABLED 1
+
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+# Expose port 3000
+EXPOSE 3000
+
+# Build the app
+RUN npm run build
+
+# Run the app
+CMD [ "npm", "start" ]
+
+# docker build -t asdr-t3 .
--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.mjs");
+await import('./src/env.mjs')
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -15,9 +15,14 @@ const config = {
    * @see https://github.com/vercel/next.js/issues/41980
    */
   i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
+    locales: ['en'],
+    defaultLocale: 'en',
   },
-};
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default config;
+export default config
