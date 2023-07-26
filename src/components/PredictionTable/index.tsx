da362cc1593371505652eaa0532857aab93770cc
klinsc <klinscea@gmail.com>
2023-07-26 17:26:51 +0700

    Update index.tsx

--- src/components/PredictionTable/index.tsx
+++ src/components/PredictionTable/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'antd'
+import { Button, Col, Row, Image, Space } from 'antd'
 import type {
   DrawingComponent,
   LineType,
@@ -10,40 +10,53 @@ import LineTypeTable from './LineTypeTable'
 import MissingComponentTable from './MissingComponentTable'
 import RemainingComponentTable from './RemainingComponentTable'
 
+interface PredictionTableProps {
+  lineTypes: LineType[]
+  drawingComponents: DrawingComponent[]
+  missingComponents: MissingComponent[]
+  remainingComponents: RemainingComponent[]
+  csvUrl: string
+  jsonUrl: string
+}
+
 const PredictionTable = ({
   lineTypes,
-  setLineTypes,
   drawingComponents,
-  setDrawingComponents,
   missingComponents,
-  setMissingComponents,
   remainingComponents,
-  setRemainingComponents,
-}: {
-  lineTypes: LineType[]
-  setLineTypes: (lineTypes: LineType[]) => void
-  drawingComponents: DrawingComponent[]
-  setDrawingComponents: (drawingComponents: DrawingComponent[]) => void
-  missingComponents: MissingComponent[]
-  setMissingComponents: (missingComponents: MissingComponent[]) => void
-  remainingComponents: RemainingComponent[]
-  setRemainingComponents: (remainingComponents: RemainingComponent[]) => void
-}) => {
+  csvUrl,
+  jsonUrl,
+}: PredictionTableProps) => {
   return (
-    <Row justify="center" align="top" gutter={[16, 16]}>
-      <Col>
-        <DrawingComponentTable drawingComponents={drawingComponents} />
-      </Col>
-      <Col>
-        <LineTypeTable lineTypes={lineTypes} />
-      </Col>
-      <Col>
-        <RemainingComponentTable remainingComponents={remainingComponents} />
-      </Col>
-      <Col>
-        <MissingComponentTable missingComponents={missingComponents} />
-      </Col>
-    </Row>
+    <>
+      <Space style={{ width: '100%', justifyContent: 'space-end' }}>
+        {csvUrl && (
+          <Button type="link" href={csvUrl} target="_blank" rel="noopener noreferrer">
+            Download CSV
+          </Button>
+        )}
+        {jsonUrl && (
+          <Button type="link" href={jsonUrl} target="_blank" rel="noopener noreferrer">
+            Download JSON
+          </Button>
+        )}
+      </Space>
+
+      <Row justify="center" align="top" gutter={[16, 16]}>
+        <Col>
+          <DrawingComponentTable drawingComponents={drawingComponents} />
+        </Col>
+        <Col>
+          <LineTypeTable lineTypes={lineTypes} />
+        </Col>
+        <Col>
+          <RemainingComponentTable remainingComponents={remainingComponents} />
+        </Col>
+        <Col>
+          <MissingComponentTable missingComponents={missingComponents} />
+        </Col>
+      </Row>
+    </>
   )
 }
 
