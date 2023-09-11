0afcd5d677ebebe97085d6cc3f334d876e4fc69c
klinsc <klinscea@gmail.com>
2023-09-11 16:51:05 +0700

    added: ANTD's theme configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
+import { type Session } from 'next-auth'
+import { SessionProvider } from 'next-auth/react'
+import { type AppType } from 'next/app'
+import { api } from '~/utils/api'
+import '~/styles/globals.css'
+import { ConfigProvider } from 'antd'
+import theme from '~/theme/themeConfig'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,9 +12,11 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ConfigProvider theme={theme}>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default api.withTRPC(MyApp);
+export default api.withTRPC(MyApp)
