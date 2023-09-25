576b5e4c80ceafe7b0c862a38b6d9be7cd3aca8c
klinsc <klinscea@gmail.com>
2023-09-25 17:02:58 +0700

    fixed: logging of ADMIN

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,47 +1,73 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { Button, Form, Input } from 'antd'
+import { Button, Col, Form, Input, Row, message } from 'antd'
 import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 const LoginPage = () => {
+  //router
+  const router = useRouter()
+  const { callbackUrl } = router.query
+
+  // messageAPI
+  const [messageAPI, contextHolder] = message.useMessage()
+
   const onFinish = (values: { username: string; password: string }) => {
-    void signIn('credentials', {
+    signIn('credentials', {
       username: values.username,
       password: values.password,
-      callbackUrl: '/',
+      // callbackUrl: (callbackUrl as string) ?? '/',
     })
+      .then(() => {
+        void router.push((callbackUrl as string) ?? '/')
+      })
+      .catch((error) => {
+        if (error instanceof Error) void messageAPI.error(error.message)
+      })
   }
 
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}>
-      <Form.Item
-        name="username"
-        rules={[{ required: true, message: 'Please input your Username!' }]}>
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Username"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[{ required: true, message: 'Please input your Password!' }]}>
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
+    <Row justify="center" align="middle">
+      {contextHolder}
+      <Col span={6} style={{ textAlign: 'center' }}>
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}>
+          <Form.Item
+            name="username"
+            rules={[
+              { required: true, message: 'Please input your Username!' },
+            ]}>
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: 'Please input your Password!' },
+            ]}>
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button">
+              Log in
+            </Button>
+            Or <a href="">register now!</a>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   )
 }
 
